335db8c0ee33a4515b0e90d3fab33c9afe257868
nach96 <n.cavero.juste@gmail.com>
2022-02-12 21:11:19 +0100

    First commit

---
+++ docs/src/index.md
@@ -0,0 +1,8 @@
+# OptimalLapTime.jl
+
+Documentation for OptimalLapTime.jl
+
+```@docs
+OptimalLapTime.greet()
+goodbye(who)
+```
---
+++ src/OptimalLapTime.jl
@@ -0,0 +1,16 @@
+module OptimalLapTime
+export goodbye
+
+""" A greet function
+# Arguments
+- None
+"""
+function greet()
+    print("Hello World!")
+end
+
+"""A goodbye function
+"""
+goodbye(who) = print("Bye "*who)
+
+end # module
